38985e1ff1d1d4969fda5f3a54349841b2ebfc9f
UgoMare <ugo.mare.epitech@gmail.com>
2016-11-24 18:27:04 +0100

    finish app

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,7 @@
 class Cocktail < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
+
+  has_many :doses
+  has_many :ingredients, through: :doses
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'cocktails#index'
-  resources :cocktails, only: [:show, :new, :create]
+  resources :cocktails, only: [:show, :new, :create] do
+    resources :doses, only: [:create]
+  end
+  resources :doses, only: [:destroy]
 end
+
+
+#/cocktails/:cocktail_id/doses/new
